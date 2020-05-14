1bc146b224130f956ecde3b6f91e8eca0b180efd
LuluAllende19 <abigail_lucii@hotmail.com>
2020-05-14 10:11:27 -0300

    Actualizando requerimientos

--- src/index.js
+++ src/index.js
@@ -13,8 +13,6 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css'; // Archivo CSS de
 import '../node_modules/bootstrap/dist/js/bootstrap.min.js'; // Archivo Javascript de Bootstrap 4 
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 
-
-
 ReactDOM.render(
   <Router>
   <div>
